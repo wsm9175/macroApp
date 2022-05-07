ff58a8a7ce96f47ac8aefe5aefd1c690d1b3999f
wsm9175 <76678911+wsm9175@users.noreply.github.com>
2022-05-08 02:52:02 +0900

    connection realtimeDB

---
+++ app/src/main/java/com/example/android/autoclick/model/User.java
@@ -0,0 +1,24 @@
+package com.example.android.autoclick.model;
+
+public class User {
+    private Long date;
+    private String location;
+
+    public User(){}
+
+    public Long getDate() {
+        return date;
+    }
+
+    public void setDate(Long date) {
+        this.date = date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
